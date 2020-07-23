a27ede9f7a97206d7710769f7c2214d91fe5376a
lordsha0 <32988899+lordsha0@users.noreply.github.com>
2020-07-22 23:24:51 -0400

    Add files via upload

---
+++ guessTheWord.py
@@ -0,0 +1,23 @@
+#! /usr/bin/python3
+
+chosenWord = "Krasivaya"
+wordIsFound = False
+print("We have chosen a word, it's up to you to uncover it!")
+while(not(wordIsFound)):
+	print("Guess it!")
+	userWord = input()
+	if(userWord == chosenWord):
+		wordIsFound = True
+		print("Congrats, you found it!")
+	else:
+                nbLettersCorrect = 0
+                if(len(userWord) == len(chosenWord)):
+                    for i in range(0,len(userWord)):
+                        if(userWord[i] == chosenWord[i]):
+                            nbLettersCorrect += 1
+                    print(f"Wrong word but you have {nbLettersCorrect} correct letters!")
+                elif(len(userWord) < len(chosenWord)):
+                    print("Your word is too short...")
+                else:
+                    print("Your word is too long...")
+                print("Try again!")
