dce1040416f34aac6694ea85cde4e963532e57a0
lordsha0 <32988899+lordsha0@users.noreply.github.com>
2020-11-04 15:18:17 -0500

    Update guessTheWord.py

--- guessTheWord.py
+++ guessTheWord.py
@@ -2,15 +2,18 @@
 
 chosenWord = "Krasivaya"
 wordIsFound = False
+
 print("We have chosen a word, it's up to you to uncover it!")
 while(not(wordIsFound)):
 	print("Guess it!")
 	userWord = input()
+	
 	if(userWord == chosenWord):
 		wordIsFound = True
 		print("Congrats, you found it!")
 	else:
                 nbLettersCorrect = 0
+		
                 if(len(userWord) == len(chosenWord)):
                     for i in range(0,len(userWord)):
                         if(userWord[i] == chosenWord[i]):
@@ -20,4 +23,5 @@ while(not(wordIsFound)):
                     print("Your word is too short...")
                 else:
                     print("Your word is too long...")
+		
                 print("Try again!")
